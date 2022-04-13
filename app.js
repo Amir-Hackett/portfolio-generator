27d34ef4862ca1b4623406180f29297faf7f2600
Amir <amir.hackett@gmail.com>
2022-04-13 17:40:48 -0400

    created function that receives command-line arguments

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 //process is a global object that represents everything going on with this particular app
 //argv property of process is an array that holds exactly what was typed into the command line upon execution
-//const profileDataArgs = process.argv.slice(2, process.argv.length)
-//console.log(profileDataArgs)
+const profileDataArgs = process.argv.slice(2, process.argv.length)
+const [name, github] = profileDataArgs;
 
-
-// Notice the lack of parentheses around the 'profileDataArr parameter!?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1){
-        console.log(profileDataArr[i])
-    } 
-
-    console.log('================')
-
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-
+//makes line break by placing on a new line
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `
 }
+console.log(name, github);
+console.log(generatePage(name, github));
