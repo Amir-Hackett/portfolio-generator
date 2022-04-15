ac6864901cc0d5a84ce0366228b81a30d93e5304
Amir <amir.hackett@gmail.com>
2022-04-15 17:58:07 -0400

    added more validation for specific questions:

--- app.js
+++ app.js
@@ -43,9 +43,23 @@ const promptUser = () => {
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself'
+      message: 'Provide some information about yourself',
+      // when is like a validation but it passes an object 
+      when: ({confirmAbout}) => {
+        if(confirmAbout){
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   ]);
 };
